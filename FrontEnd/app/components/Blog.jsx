60918d14052e1c081ec709b40b9b7e6ddf10b566
chen.zhang <chen.zhang@alibaba-inc.com>
2016-02-03 11:53:19 +0800

    commit unversioned files

---
+++ FrontEnd/app/components/Blog.jsx
@@ -0,0 +1,13 @@
+/**
+ * use this component to form a blog for show in blog site
+ *
+ */
+
+import React from 'react';
+
+export default class Blog extends React.Component {
+    render() {
+        return <div>this is a blog conponent </div>;
+    }
+}
+
